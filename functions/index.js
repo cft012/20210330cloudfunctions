dec6dc83e97a6e84a53e7bc6fb7a5a56f55fc1ed
cft012 <tk.suzuki08@gmail.com>
2021-04-06 20:36:22 +0900

    first commit

---
+++ functions/index.js
@@ -0,0 +1,61 @@
+// index.js
+const functions = require("firebase-functions");
+const express = require("express");
+const requestPromise = require("request-promise-native"); // 追加
+const cors = require('cors');
+
+const app = express();
+
+// app.use(cors());
+
+// APIにリクエストを送る関数を定義
+const getDataFromApi = async (keyword) => {
+  // cloud functionsから実行する場合には地域の設定が必要になるため，`country=JP`を追加している
+  const requestUrl =
+    "https://www.googleapis.com/books/v1/volumes?country=JP&q=intitle:";
+  const result = await requestPromise(`${requestUrl}${keyword}`);
+  return result;
+};
+
+// local
+// http://localhost:5000/functions-881e3/us-central1/helloWorld
+
+// deploy
+// https://us-central1-functions-881e3.cloudfunctions.net/api
+
+
+
+app.get("/hello", (req, res) => {
+  // レスポンスの設定 o
+  res.send("Hello Express!");
+});
+
+app.get("/user/:userId", (req, res) => {
+  const users = [
+    { id: 1, name: "ジョナサン" },
+    { id: 2, name: "ジョセフ" },
+    { id: 3, name: "承太郎" },
+    { id: 4, name: "仗助" },
+    { id: 5, name: "ジョルノ" },
+  ];
+  // req.params.userIdでURLの後ろにつけた値をとれる．
+  const targetUser = users.find(
+    (user) => user.id === Number(req.params.userId)
+  );
+  res.send(targetUser);
+});
+
+// エンドポイント追加
+app.get("/gbooks/:keyword", cors(), async (req, res) => {
+  // APIリクエストの関数を実行
+  const response = await getDataFromApi(req.params.keyword);
+  res.send(response);
+});
+
+const api = functions.https.onRequest(app);
+module.exports = { api };
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   // functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
